5d59bcf4f119ebec68b93f6e6f5a967d525972ae
Protsenko Arseny <procenko_03@mail.ru>
2021-05-06 21:57:39 +0500

    group2vec notebook and add TODOs to executor

---
+++ README.md
@@ -0,0 +1,2 @@
+# Suvec
+### Pretrained models are [here](https://drive.google.com/drive/folders/1L_cHapEISPOgUohZN7Tt84f3kj5OHl41?usp=sharing).
--- suvec/vk_api_impl/executing/pool_executor.py
+++ suvec/vk_api_impl/executing/pool_executor.py
@@ -14,6 +14,8 @@ class VkApiPoolExecutor(AccessErrorListener):
         self.session_manager = session_manager
 
     def execute(self, requests: List[Request]) -> List[Response]:
+        # TODO: need fast check for access error and other errors when should break executing.
+        #   Otherwise have large overheads
         session = self.session_manager.get_session()
         responses = []
         for idx, req in enumerate(requests):
@@ -21,12 +23,17 @@ class VkApiPoolExecutor(AccessErrorListener):
             responses.append(req.create_response(resp_raw))
             self._execute_pool_if_needed(session, idx)
 
-        session.execute()
+        self._execute(session)
         return responses
 
     def _execute_pool_if_needed(self, session: VkRequestsPool, idx: int):
         if (idx + 1) % self.max_pool_size == 0:
-            session.execute()
+            self._execute(session)
+
+    def _execute(self, session):
+        # TODO: handle this error, that can occur: RemoteDisconnected('Remote end closed connection without response')
+        # TODO: handle JSONDecodeError: Expecting value: line 1 column 247645 (char 247644)
+        session.execute()
 
     def access_error_occurred(self, *args, **kwargs):
         self.session_manager.reset_session_access_error()
