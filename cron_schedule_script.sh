cd30da4f48bc83449c45a8db5bb5110c388bd355
Protsenko Arseny <protsenkoar@gmail.com>
2021-08-20 18:43:13 +0500

    fixing cron

--- cron_schedule_script.sh
+++ cron_schedule_script.sh
@@ -1,3 +1,4 @@
-cd Projects/skady-user-vectorizer
+export PATH="$HOME/.local/bin:$PATH"
+cd $HOME/Projects/skady-user-vectorizer/
+printenv >> theenv.txt
 pipenv run python3 ./schedule_main_runs.py
-
